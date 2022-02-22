3b4efadd0698f08335a869c261d153d5f25bf036
Rob <robzlegz@gmail.com>
2022-02-22 16:17:32 +0200

    models

---
+++ src/models/userModel.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        default: ""
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    course: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    rtk_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    avatar: {
+        type: String,
+        default: "https://res.cloudinary.com/ddqddw48c/image/upload/v1645539247/rtkdzive/avatars/user_g8pwsh.svg"
+    },
+    friends: {
+        type: Array,
+        default: []
+    },
+}, {
+    timestamps: true
+});
+
+let Dataset = mongoose.models.user || mongoose.model("user", userSchema);
+export default Dataset;
