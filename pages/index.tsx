f1ac6f9c9a95b90e172e5aacaa8f3a0825507f3f
Rob <robzlegz@gmail.com>
2022-02-23 11:12:51 +0200

    logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,7 @@
 import Head from "next/head"
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import Navigation from "../src/components/Navigation";
-import { selectUser, UserInfo } from "../src/redux/slices/userSlice";
-import { checkForLogin } from "../src/requests/userRequests";
 
 export default function Home() {
-  const userInfo: UserInfo = useSelector(selectUser);
-
-  const dispatch = useDispatch();
-  const router = useRouter();
-
-  useEffect(() => {
-    if(!userInfo.loggedIn || !userInfo.token){
-      const token = window.localStorage.getItem("refreshtoken");
-
-      if(token){
-        checkForLogin(dispatch, router);
-      }else{
-        router.push("/auth/login");
-      }
-    }
-  }, [userInfo.loggedIn, dispatch, userInfo.token, router]);
-
   return (
     <div>
       <Head>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,27 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser, UserInfo } from "../redux/slices/userSlice";
+import { checkForLogin } from "../requests/userRequests";
 
 function Navigation() {
+    const userInfo: UserInfo = useSelector(selectUser);
+
+    const dispatch = useDispatch();
     const router = useRouter();
+  
+    useEffect(() => {
+        if(!userInfo.loggedIn || !userInfo.token){
+            const token = window.localStorage.getItem("refreshtoken");
+    
+            if(token){
+                checkForLogin(dispatch, router);
+            }else{
+                router.push("/auth/login");
+            }
+        }
+    }, [userInfo.loggedIn, dispatch, userInfo.token, router]);
 
     const [type] = useState(router.pathname);
 
@@ -15,8 +34,29 @@ function Navigation() {
     }
     
     return (
-        <nav className="w-full h-14 flex items-center justify-between px-5 fixed top-0 left-0">
-            <h1>RTK dzīve</h1>
+        <nav className="w-full h-14 flex items-center justify-between px-5 fixed top-0 left-0 bg-rtkBlue text-white">
+            <Link href="/">
+                <h1 className="mr-4 text-white cursor-pointer">RTK dzīve</h1>
+            </Link>
+
+            <input 
+                className="flex-1 h-8"
+                type="text" 
+                name="" 
+                id="" 
+            />
+
+            <div className="flex ml-4">
+                <Link href="/">
+                    <p className="cursor-pointer mr-2">Home</p>
+                </Link>
+
+                <Link href="/friends">
+                    <p className="cursor-pointer">Friends</p>
+                </Link>
+            </div>
+
+
         </nav>
     )
 }
