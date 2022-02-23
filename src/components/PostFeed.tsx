4f8a2e254a092574f432355c27792457a1118970
Rob <robzlegz@gmail.com>
2022-02-23 12:13:55 +0200

    svg

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import postData from '../data/postData';
+import Post from './Post';
 
 function PostFeed() {
   return (
-    <div>PostFeed</div>
+    <div className="w-[500px]">
+        <div className="bg-white w-full mb-4">
+
+        </div>
+
+        <div className="bg-white p-2 w-full">
+            {
+                postData.map((p, i) => {
+                    return (
+                        <Post 
+                            key={i}
+                            data={p}
+                        />
+                    )
+                })
+            }
+        </div>
+    </div>
   )
 }
 
